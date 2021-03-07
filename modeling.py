30708362f57f8ccb395fdf05346c73b479957de3
Coldwater30 <40321794+Coldwater30@users.noreply.github.com>
2021-03-07 17:55:10 +0800

    add TBHG class

--- modeling.py
+++ modeling.py
@@ -33,11 +33,84 @@ class CNode:
             return True
         return False
 
-    def getCNode(self, sp) -> CNode:
-        for c in self.clusters:
-            if c.isIn(sp):
-                return c
 
+class TBHG:
+    def __init__(self, optics: OPTICS, locH):
+        self.optics = optics
+        self.hierarchy = self.__buildHierarchy(self.__buildHTree(self))
+        self.__buildGraph(self)
+
+    def __buildHierarchy(self, r: CNode):
+        h = []   
+        level = [r]
+        while level:
+            h.append(level)
+            children = []
+            for c in level:
+                children.extend(c.children)
+            level = children
+        return h
+
+
+    
+
+
+    def __buildGraph(self):
+        """build graph on a collection of SP clusters
+        """
+        #** compute mapping: locH -> ordering_
+        ordering = self.optics.ordering_
+        locH = np.ndarray.flatten(self.locH)
+        
+        orderOfSPs = np.zeros_like(ordering)
+        orderOfSPs[ordering] = np.arange(0, ordering.size)
+
+        def getCNode(level, sp):
+            for c in level:
+                if sp in c:
+                    return c
+            else:
+                return None
+                
+        for l in self.hierarchy:
+            n = len(l)  
+            maxEdgeCount = n*(n-1)
+            c = 0
+
+            prev = getCNode(l, orderOfSPs[locH[0]])   
+            for s in locH and c < maxEdgeCount:
+                temp = getCNode(l, orderOfSPs[s])   
+                curr = temp if temp else curr                
+                if prev is not curr:
+                    #build edge
+                    prev.addNeighbor(curr)
+                    c += 1
+                prev = curr
+
+    def __buildHTree(self, optics: OPTICS):
+        #**
+        cIter = iter(optics.cluster_hierarchy[::-1])
+        r = CNode([0, len(optics.ordering_)])
+        try:
+            cn = CNode(next(cIter))
+            while True:
+                while cn in r:
+                    r.addChild(cn)
+                    cn = self.__buildHTree_helper(self, cn, cIter)
+                # for debug    
+                else:
+                    print("false")
+        except StopIteration:
+            return r
+
+    def __buildHTree_helper(self, r, cIter):
+        while True:
+            cn = CNode(next(cIter))
+            while cn in r:
+                r.addChild(cn)
+                cn = self.__buildHTree_helper(self, cn, cIter)
+            else:
+                return cn 
 
 def detectStayPoints(traj: np.ndarray, tThresh, dThresh):
     """Detect stay points in a trajectory
