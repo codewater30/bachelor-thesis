d58d816e6ab2ce5f00da4258261b56c0b74007e5
Coldwater30 <40321794+Coldwater30@users.noreply.github.com>
2021-03-25 10:42:37 +0800

    feat: add buildMatrix()

--- inferencing.py
+++ inferencing.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 from modeling import TBHG
 from modeling import CNode
+
 def collectLocation(searchDepth, cluster: CNode):
     children = [cluster]
     for i in range(searchDepth):
@@ -11,3 +14,25 @@ def collectLocation(searchDepth, cluster: CNode):
 
     return children
 
+def buildMatrix(clusters: np.ndarray[CNode], locH, ordering):
+    hisLens = [len(h) for h in locH]
+
+    #init matrix 
+    matrix = np.zeros((len(locH), len(clusters)))
+
+
+    for i, c in enumerate(clusters):
+        start, end = c.cluster
+        indices = np.zeros((np.size(locH),))
+
+        #mark occurrences 
+        indices[ordering[start:end+1]] = 1
+
+        #count times of visit to the location for each user
+        s = e = 0
+        vector = []
+        for l in hisLens:
+            s, e = e, l 
+            vector.append(sum(indices[s:e]))
+
+        matrix[:, i] = vector
