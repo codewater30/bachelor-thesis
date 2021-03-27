a7d09401791dff91ceb3fb1d6171b2dc4cbfc045
Coldwater30 <40321794+Coldwater30@users.noreply.github.com>
2021-03-27 15:37:26 +0800

    feat: add util funcs:
    - plt_clusters
    - get_trace_files
    - print_hierarchy
    - build_hierarchy
    - build_tree

---
+++ util.py
@@ -0,0 +1,32 @@
+import os, re
+from matplotlib import pyplot as plt
+from sklearn.cluster import OPTICS
+
+import numpy as np
+import modeling
+def plt_clusters(clusters: OPTICS, samples):
+    n_lables = np.amax(clusters.labels_)
+    cmap = plt.get_cmap("viridis", n_lables)
+    for i in range(n_lables):
+        x = samples[clusters.labels_ == i]
+        plt.scatter(x[:, 0], x[:, 1], color=cmap(i), alpha=0.5)
+    x = samples[clusters.labels_ == -1]
+    plt.scatter(x[:, 0], x[:, 1], alpha=0.1)
+    plt.show()
+    plt.close()
+    
+def get_trace_files(path):
+    return [os.path.join(path, trace) for trace in os.listdir(path) if re.match(r'\d+\.trace',trace)]
+
+def print_hierarchy(h):
+    for level in h:
+        print(level)
+
+def build_hierarchy(optics):
+    tbhg = modeling.TBHG(optics)
+    return tbhg.hierarchy
+
+def build_tree(optics):
+    tbhg = modeling.TBHG()
+    tbhg.optics = optics
+    return tbhg._build_tree()
